64a02977a1b1101f0eec7acd9a02cb5c252ca2be
Javeria Kanwal <109088965+Javeriach@users.noreply.github.com>
2024-10-15 23:41:29 +0500

    Time: 110 ms (74.78%), Space: 62.4 MB (87.64%) - LeetHub

--- 0969-number-of-recent-calls/0969-number-of-recent-calls.cpp
+++ 0969-number-of-recent-calls/0969-number-of-recent-calls.cpp
@@ -7,6 +7,14 @@ public:
     }
     
     int ping(int t) {
+    
+    // Login:
+    //1---if given time t is less than 3000 then simply add and return the queue size because they always come in i
+    //in increasing order --agr given time he 3000 sy kum hy meand ky jitny be prhly number hn wo sub 3000 sy kum he hn gan
+
+    //2---agr given time he  3000 sy bary hy tu means ky tu hamy bus wahe add karny hn jo ky margin ky ander ander hn gan
+    //margin = giventime(t) - 3000; is liya margin sy kum jitny be numbers hn gan hm wo sary queue sy nikal dan gan.
+    //remeber they are already in sorted order....is liya hum apny margin sy pehly jitny number h un sub ko nikal dan gan
 
         if(t <= 3000){
             myQueue.push(t);
