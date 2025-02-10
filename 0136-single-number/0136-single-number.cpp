35aa83fae799f6b95ccfb07ef73a8b25a7640d25
Javeria Kanwal <109088965+Javeriach@users.noreply.github.com>
2025-02-10 16:48:17 +0500

    Time: 12 ms (14.66%), Space: 24 MB (7.19%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+
+        unordered_map<int,int>result={{0,0}};
+        for(int i=0; i<nums.size();i++)
+        {
+            if(result[nums[i]])
+            result[nums[i]]=result[nums[i]]+1;
+            else 
+            result[nums[i]]=1;
+        }
+        
+        for(int i=0; i<nums.size();i++)
+        {
+            if(result[nums[i]] == 1)
+            {
+                return nums[i];
+            }
+        }
+
+        return -1;
+    }
+};
