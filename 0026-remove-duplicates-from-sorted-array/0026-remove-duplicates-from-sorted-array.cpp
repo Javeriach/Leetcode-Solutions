1f4bfe188e10bf666ca9ce8e71f03a0604df1569
Javeria Kanwal <109088965+Javeriach@users.noreply.github.com>
2025-02-10 18:09:33 +0500

    Time: 6 ms (8.98%), Space: 23.4 MB (8.18%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -4,25 +4,27 @@ public:
     int removeDuplicates(vector<int>& nums) {
         unordered_map<int,int>map;
         
+        int j=0;
         for(int i=0; i<nums.size();i++)
         {
             if(map.find(nums[i]) == map.end())
             {
                 map[nums[i]]=nums[i];
-                cout << map[nums[i]]<<endl;
+                nums[j]=nums[i];
+                j++;
             }
         }
         
         
-        int k=0;
-        for(auto it:map)
-        {
-            // cout << it.second <<endl;
-            nums[k]=it.second;
-            k++;
-        }
+        // int k=0;
+        // for(auto it:map)
+        // {
+        //     // cout << it.second <<endl;
+        //     nums[k]=it.second;
+        //     k++;
+        // }
 
-        sort(nums.begin(),nums.begin()+map.size());
+        // sort(nums.begin(),nums.begin()+map.size());
        
         
     return map.size();
