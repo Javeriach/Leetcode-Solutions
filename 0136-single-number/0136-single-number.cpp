600a87622eb4c990cbbff2184d3e125f8d0e3827
Javeria Kanwal <109088965+Javeriach@users.noreply.github.com>
2025-02-10 17:32:03 +0500

    Time: 8 ms (20.29%), Space: 24 MB (10.56%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -6,10 +6,8 @@ public:
         unordered_map<int,int>result={{0,0}};
         for(int i=0; i<nums.size();i++)
         {
-            if(result[nums[i]])
-            result[nums[i]]=result[nums[i]]+1;
-            else 
-            result[nums[i]]=1;
+            result[nums[i]]+=1;
+            
         }
         
         for(int i=0; i<nums.size();i++)
