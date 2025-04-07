a56f9807fb14a9df6148ccaa7e9f02cc86b7c546
Javeria Kanwal <109088965+Javeriach@users.noreply.github.com>
2025-04-07 15:12:34 +0500

    Time: 0 ms (100%), Space: 10.6 MB (94.05%) - LeetHub

--- 1833-find-the-highest-altitude/1833-find-the-highest-altitude.cpp
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.cpp
@@ -2,20 +2,14 @@ class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
 
-        int max=0;
-        vector<int>resultArray;
-        resultArray.push_back(0);
-         int temp=0;
-
+        int maxSum=0;
+        int curSum=0;
         for(int i=0; i < gain.size(); i++)
         {
-                int sum = gain[i] + resultArray[temp];
-                cout << gain[i] << " "  << resultArray[temp] << " " << sum <<endl;
-                temp++;
-                if(sum > max) max=sum;
-                resultArray.push_back(sum);
+            curSum+=gain[i];
+            maxSum=max(curSum,maxSum);
         }
  
-    return max;
+    return maxSum;
     }
 };
