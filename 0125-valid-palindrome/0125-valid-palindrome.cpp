9dc39cf411bfe71a3ebb0b0a51340ea9d2c01cf7
Javeria Kanwal <109088965+Javeriach@users.noreply.github.com>
2025-04-13 01:17:19 +0500

    Time: 1 ms (48.71%), Space: 10.7 MB (6.26%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        
+        string temp;
+        for(int i=0; i<s.length();i++)
+        {
+            if((s[i] >= 'a'  && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= '0' && s[i] <= '9')) {
+                temp.push_back(tolower(s[i]));
+                };
+
+        }
+       cout << temp <<endl;
+       string originalStr=temp;
+       reverse(temp.begin(),temp.end());
+      
+        return originalStr == temp;
+    }
+};
