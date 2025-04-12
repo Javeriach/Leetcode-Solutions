e412eb5f6fe29395995f9fc58f03fd163d0021eb
Javeria Kanwal <109088965+Javeriach@users.noreply.github.com>
2025-04-12 15:06:56 +0500

    Time: 12 ms (17.79%), Space: 13.2 MB (47.12%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        int sum=0;
-        unordered_map<char,int>map={
-            {'I',1},
-             {'V',5},
-              {'X',10},
-               {'L',50},
-                {'C',100},
-                 {'D',500},
-            {'M',1000},
 
-        };
+      int sum=0;
 
-        for(int i=0; i<s.length()-1;i++)
-        {
-            if(map[s[i]] < map[s[i+1]])
+      unordered_map<char,int>roman={
+        {'I',1},{'V',5},{'X',10},
+        {'L',50},{'C',100},{'D',500},{'M',1000}
+      };
+      int i=0;
+      for(i=0; i<s.length()-1;i++)
+      {
+            if(roman[s[i]] >= roman[s[i+1]]) 
             {
-                sum-=map[s[i]];
-            }else
-            {
-                sum+=map[s[i]];
+                sum+=roman[s[i]];
+              
+
             }
-
-        }
-
-         sum+=map[s[s.length()-1]];
-
-         return sum;
+            else 
+           { 
+            sum=sum + (roman[s[i+1]] - roman[s[i]]);
+            i++;
+           }
+      }
+  
+     if(i < s.length())
+      sum+=roman[s[s.length()-1]];
+     
+      return sum;
     }
 };
