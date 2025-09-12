d1aa0a8564107af13ddd9f7176250c5ccffdbb5f
Javeria Kanwal <109088965+Javeriach@users.noreply.github.com>
2025-09-12 10:51:01 +0500

    Time: 0 ms (100%), Space: 16.6 MB (3.5%) - LeetHub

--- 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.cpp
+++ 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.cpp
@@ -2,44 +2,38 @@ class Solution {
 public:
     int findGCD(vector<int>& nums) {
 
-       sort(nums.begin(), nums.end());
-       int num1=nums.front();
-       int num2=nums.back();
-       
-       unordered_set<int> divisors1=Divisors(num1);
-       unordered_set<int> divisors2=Divisors(num2);
-       vector<int>common_result;
+        sort(nums.begin(), nums.end());
+        int num1 = nums.front();
+        int num2 = nums.back();
 
-       for(int d : divisors1 )
-       {
-           if(divisors2.find(d) != divisors2.end())
-            {
-                    common_result.push_back(d);
+        unordered_set<int> divisors1;
+        unordered_set<int> divisors2;
+        vector<int> common_result;
+
+        for (int i = 1; i <= num1; i++) {
+            if (num1 % i == 0 && divisors1.find(i) == divisors1.end()) {
+                divisors1.insert(i);
             }
-       }
-       if(common_result.size() == 0)
-       {
-          return 1;
-       }
+        }
 
-    sort(common_result.begin(),common_result.end());
-     return common_result.back();
+
+        for (int i = 1; i <= num1; i++) {
+            if (num2 % i == 0 && divisors2.find(i) == divisors2.end()) {
+                divisors2.insert(i);
+            }
+        }
+
+        for (int d : divisors1) {
+            if (divisors2.find(d) != divisors2.end()) {
+                common_result.push_back(d);
+            }
+        }
+
+        if (common_result.size() == 0) {
+            return 1;
+        }
+
+        sort(common_result.begin(), common_result.end());
+        return common_result.back();
     }
-private:
-
- unordered_set<int> Divisors(int num)
- {
-    unordered_set<int>divisors;
-
-     for(int i=1; i<=num;i++)
-     {
-         if(num % i == 0 && divisors.find(i) == divisors.end())
-         {
-            divisors.insert(i);
-         }
-     }
-
-     return divisors;
- }
-    
 };
