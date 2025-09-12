736b88dd74568422519a84e1d0831a83d548d5e5
Javeria Kanwal <109088965+Javeriach@users.noreply.github.com>
2025-09-12 09:42:04 +0500

    Time: 3 ms (7.18%), Space: 16.5 MB (3.5%) - LeetHub

--- 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.cpp
+++ 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.cpp
@@ -1,35 +1,45 @@
 class Solution {
 public:
     int findGCD(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n1 = nums.front();  // smallest
-        int n2 = nums.back();   // largest
 
-        unordered_set<int> divisors1 = getDivisors(n1);
-        unordered_set<int> divisors2 = getDivisors(n2);
-        vector<int> common_factors;
+       sort(nums.begin(), nums.end());
+       int num1=nums.front();
+       int num2=nums.back();
+       
+       unordered_set<int> divisors1=Divisors(num1);
+       unordered_set<int> divisors2=Divisors(num2);
+       vector<int>common_result;
 
-        for (int d : divisors1) {
-            if (divisors2.find(d) != divisors2.end()) {
-                common_factors.push_back(d);
+       for(int d : divisors1 )
+       {
+           if(divisors2.find(d) != divisors2.end())
+            {
+                    common_result.push_back(d);
             }
-        }
+       }
+       if(common_result.size() == 0)
+       {
+          return 1;
+       }
 
-        if (common_factors.empty()) return 1; // in worst case, 1 is always a common divisor
-
-        sort(common_factors.begin(), common_factors.end());
-        return common_factors.back(); // largest common divisor
+    sort(common_result.begin(),common_result.end());
+     return common_result.back();
     }
-
 private:
-    unordered_set<int> getDivisors(int n) {
-        unordered_set<int> divisors;
-        for (int i = 1; i * i <= n; ++i) {
-            if (n % i == 0) {
-                divisors.insert(i);
-                divisors.insert(n / i);
-            }
-        }
-        return divisors;
-    }
+
+ unordered_set<int> Divisors(int num)
+ {
+    unordered_set<int>divisors;
+
+     for(int i=1; i<=num;i++)
+     {
+         if(num % i == 0 && divisors.find(i) == divisors.end())
+         {
+            divisors.insert(i);
+         }
+     }
+
+     return divisors;
+ }
+    
 };
