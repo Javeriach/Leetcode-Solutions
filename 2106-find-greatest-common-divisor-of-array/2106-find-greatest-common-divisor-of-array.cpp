0bb27e2a18df5adb1510d17f224d34410e55d3c0
Javeria Kanwal <109088965+Javeriach@users.noreply.github.com>
2025-09-12 17:00:37 +0500

    Time: 0 ms (100%), Space: 16.1 MB (65.59%) - LeetHub

--- 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.cpp
+++ 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.cpp
@@ -5,35 +5,17 @@ public:
         sort(nums.begin(), nums.end());
         int num1 = nums.front();
         int num2 = nums.back();
+        return calculateGCD(num1, num2);
+    }
 
-        unordered_set<int> divisors1;
-        unordered_set<int> divisors2;
-        vector<int> common_result;
-
-        for (int i = 1; i <= num1; i++) {
-            if (num1 % i == 0 && divisors1.find(i) == divisors1.end()) {
-                divisors1.insert(i);
-            }
+private:
+    int calculateGCD(int num1, int num2) {
+        int n1=max(num1,num2);
+        int n2=min(num1,num2);
+        if (num1 == 0) {
+            return num2;
         }
 
-
-        for (int i = 1; i <= num1; i++) {
-            if (num2 % i == 0 && divisors2.find(i) == divisors2.end()) {
-                divisors2.insert(i);
-            }
-        }
-
-        for (int d : divisors1) {
-            if (divisors2.find(d) != divisors2.end()) {
-                common_result.push_back(d);
-            }
-        }
-
-        if (common_result.size() == 0) {
-            return 1;
-        }
-
-        sort(common_result.begin(), common_result.end());
-        return common_result.back();
+        return calculateGCD(n1-n2, n2);
     }
 };
