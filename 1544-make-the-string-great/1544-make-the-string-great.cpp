f6a94d27165d248609ad8802abe59253dcc51595
Javeria Kanwal <109088965+Javeriach@users.noreply.github.com>
2026-08-01 01:30:17 +0500

    Time: 0 ms (100%), Space: 8.8 MB (41.39%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+
+    bool pop_required(char firstChar, char top_char)
+    {
+        if(tolower(firstChar) != tolower(top_char)) return false;
+
+        if(isupper(firstChar) && islower(top_char) ) return true;
+        if(islower(firstChar) && isupper(top_char) ) return true;
+
+        return false;
+    }
+    string makeGood(string s) {
+        stack<char>s_stack;
+        int i=0;
+        while(i < s.length())
+        {
+            if(!s_stack.empty() && pop_required(s[i], s_stack.top()))
+            {
+                s_stack.pop();
+            }else
+            {
+                s_stack.push(s[i]);
+            }
+            i++;
+        }
+
+        string goodString;
+        while(!s_stack.empty())
+        {
+               goodString+=s_stack.top();
+               s_stack.pop();
+        }
+
+        reverse(goodString.begin(), goodString.end());
+        return goodString;
+    }
+};
